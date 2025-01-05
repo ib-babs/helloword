20384c0e6ba4fb4dec4c290fae5e4c5125912333
ib-babs <babatundeibrahim426@gmail.com>
2025-01-05 17:28:31 +0100

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using helloword.Models;
+
+namespace helloword.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<Movie> Movies { get; set; } = [
+            new(){Title = "Journey of the Flower", Price = 10}
+            ];
+        public IActionResult Index()
+        {
+            ViewBag.Title = "HomePage";
+            return View(Movies);
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateMovie()
+        {
+            ViewBag.Title = "New";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateMovie(Movie movie)
+        {
+            ViewBag.Title = "New";
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            Movies.Add(movie);
+
+            //Console.WriteLine(movie.Price);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace helloword.Models
+{
+#pragma warning disable CS8618
+    public class Movie
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Required(ErrorMessage = "Please enter movie title")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter movie date")]
+        public DateOnly ReleasedDate { get; set; }
+
+        [Required(ErrorMessage = "Please enter movie price")]
+        public decimal Price { get; set; }
+    }
+}
